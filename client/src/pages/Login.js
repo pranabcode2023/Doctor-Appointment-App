d581a5d69543da7f643e71909bc7316f07d7cd29
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-11-21 13:38:31 +0100

    registration form and login form added

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,37 @@
 import React from "react";
-
+import { Form, Input } from "antd";
+import "../styles/RegisterStyles.css";
+import { Link } from "react-router-dom";
 const Login = () => {
+  const onfinishHandler = (values) => {
+    console.log(values);
+  };
   return (
-    <div>
-      <h1> Login</h1>
-    </div>
+    <>
+      <div className="form-container">
+        <Form
+          className="register-form"
+          layout="vertical"
+          onFinish={onfinishHandler}
+        >
+          <h3 className="text-center">Login Form</h3>
+
+          <Form.Item label="Email" name="email">
+            <Input type="email" required />
+          </Form.Item>
+
+          <Form.Item label="Password" name="password">
+            <Input type="password" required />
+          </Form.Item>
+          <Link to="/register" className="m-2">
+            Not a user? go for register
+          </Link>
+          <button className="btn btn-primary" type="submit">
+            Login
+          </button>
+        </Form>
+      </div>
+    </>
   );
 };
 
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,44 @@
 import React from "react";
+import { Form, Input } from "antd";
+import "../styles/RegisterStyles.css";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return <div>Register</div>;
+  const onfinishHandler = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <>
+      <div className="form-container">
+        <Form
+          className="register-form"
+          layout="vertical"
+          onFinish={onfinishHandler}
+        >
+          <h3 className="text-center">Register Form</h3>
+          <Form.Item label="Name" name="name">
+            <Input type="text" required />
+          </Form.Item>
+
+          <Form.Item label="Email" name="email">
+            <Input type="email" required />
+          </Form.Item>
+
+          <Form.Item label="Password" name="password">
+            <Input type="password" required />
+          </Form.Item>
+          <Link to="/login" className="m-2">
+            {" "}
+            Already registerd? go for login
+          </Link>
+          <button className="btn btn-primary" type="submit">
+            Register
+          </button>
+        </Form>
+      </div>
+    </>
+  );
 };
 
 export default Register;
