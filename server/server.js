1ee96b682358bcf7a4304458244f0057b6fe688f
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-11-26 15:18:21 +0100

    import monodb function

--- server/server.js
+++ server/server.js
@@ -2,9 +2,13 @@ const express = require("express");
 const colors = require("colors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 //dotenv config
 dotenv.config();
+
+// mongoDB connection
+connectDB();
 
 // rest object
 const app = express();
@@ -14,11 +18,14 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 //routes
-app.get("/", (req, res) => {
-  res.status(200).send({
-    message: " server running sucessfully",
-  });
-});
+app.use("/api/v1/user", require("./routes/userRoutes"));
+
+//test Purpose
+// app.get("/", (req, res) => {
+//   res.status(200).send({
+//     message: " server running sucessfully",
+//   });
+// });
 
 //port
 const port = process.env.PORT || 8080;
