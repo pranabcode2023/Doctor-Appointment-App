e7611d479116199af825c7bc11ad4559bd00051f
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-12-06 21:10:42 +0100

    function added to get user data

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,6 +1,31 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 
 const HomePage = () => {
+  // get user Data
+
+  const getUserData = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:8080/api/v1/user/getUserData ",
+        {},
+        {
+          headers: {
+            // must have one space after Bearer . read documentation
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        }
+      );
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
   return (
     <div>
       <h1> HomePage</h1>
