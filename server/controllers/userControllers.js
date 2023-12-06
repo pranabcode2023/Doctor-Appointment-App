c8c5c1effaf6f5419be6542b505ef20403ea3062
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-12-06 18:58:44 +0100

    added getUserDataController function

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -80,4 +80,31 @@ const loginController = async (req, res) => {
   }
 };
 
-module.exports = { loginController, registerController };
+const getUserDataController = async (req, res) => {
+  try {
+    const user = await userModel.findOne({ _id: req.body.userId });
+    if (!user) {
+      return res.status(200).send({
+        message: "User not found",
+        success: false,
+      });
+    } else {
+      res.status(200).send({
+        success: true,
+        data: {
+          name: user.name,
+          email: user.email,
+        },
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: "Error in  getUserDataController ",
+      success: false,
+      error,
+    });
+  }
+};
+
+module.exports = { loginController, registerController, getUserDataController };
