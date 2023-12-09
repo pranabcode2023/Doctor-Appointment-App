c4adc71b27190d0154cdfc9002d0ff13c768818c
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-12-09 11:18:13 +0100

    loading spinner added

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -4,16 +4,23 @@ import "../styles/RegisterStyles.css";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
+import { useDispatch } from "react-redux";
+import { showLoading, hideLoading } from "../redux/features/alertSlice";
+
 const Login = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   // Login form controll
 
   const onfinishHandler = async (values) => {
     try {
+      dispatch(showLoading());
       const res = await axios.post(
         "http://localhost:8080/api/v1/user/login",
         values
       );
+
+      dispatch(hideLoading());
       console.log(res);
       if (res.data.success) {
         //generate token & save into localstorage
@@ -25,6 +32,7 @@ const Login = () => {
         message.error(res.data.message);
       }
     } catch (error) {
+      dispatch(hideLoading());
       console.log(error);
       message.error("Something went wrong ");
     }
