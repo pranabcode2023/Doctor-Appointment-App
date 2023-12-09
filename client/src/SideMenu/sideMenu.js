e4de9f49826903d116651c5feb9229ecb182709f
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-12-09 11:20:43 +0100

    config for side menu added

---
+++ client/src/SideMenu/sideMenu.js
@@ -0,0 +1,27 @@
+export const sideMenu = [
+  {
+    name: "Home",
+    path: "/",
+    icon: "fa-solid fa-house-chimney-medical",
+  },
+  {
+    name: "Appointments",
+    path: "/appointments",
+    icon: "fa-solid fa-bars",
+  },
+  {
+    name: "Doctors",
+    path: "/doctors",
+    icon: "fa-solid fa-user-doctor",
+  },
+  {
+    name: "Profile",
+    path: "/profile",
+    icon: "fa-solid fa-user",
+  },
+  {
+    name: "Logout",
+    path: "/logout",
+    icon: "fa-solid fa-right-from-bracket",
+  },
+];
