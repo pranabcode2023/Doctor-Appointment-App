259596263c5adeb7f38e57be23428b107d89fd27
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-12-28 15:26:45 +0100

    userModel find by Id and get all user data

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -82,7 +82,8 @@ const loginController = async (req, res) => {
 
 const getUserDataController = async (req, res) => {
   try {
-    const user = await userModel.findOne({ _id: req.body.userId });
+    const user = await userModel.findById({ _id: req.body.userId });
+    user.password = undefined;
     if (!user) {
       return res.status(200).send({
         message: "User not found",
@@ -91,10 +92,7 @@ const getUserDataController = async (req, res) => {
     } else {
       res.status(200).send({
         success: true,
-        data: {
-          name: user.name,
-          email: user.email,
-        },
+        data: user,
       });
     }
   } catch (error) {
