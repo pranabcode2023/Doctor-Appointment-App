d853ec69ec4a5b586cb25133c281cf461c7483c7
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2024-01-04 14:42:41 +0100

    function adedd for getAllNotificationController, deleteAllNotificationController

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -138,9 +138,63 @@ const applyDoctorController = async (req, res) => {
   }
 };
 
+// get All Notification  Controller
+
+const getAllNotificationController = async (req, res) => {
+  try {
+    const user = await userModel.findOne({ _id: req.body.userId });
+    const seenNotification = user.seenNotification;
+    const notification = user.notification;
+
+    seenNotification.push(...notification);
+    user.notification = [];
+    user.seenNotification = notification;
+
+    const updatedUser = await user.save();
+    res.status(200).send({
+      success: true,
+      message: "All notification marked as read",
+      data: updatedUser,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: "Error in notification",
+      success: false,
+      error,
+    });
+  }
+};
+
+// Delete All Notification  Controller
+
+const deleteAllNotificationController = async (req, res) => {
+  try {
+    const user = await userModel.findOne({ _id: req.body.userId });
+    user.notification = [];
+    user.seenNotification = [];
+    const updatedUser = await user.save();
+    updatedUser.password = undefined;
+    res.status(200).send({
+      success: true,
+      message: "Notifications Deleted successfully",
+      data: updatedUser,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Unable to delete all Notifications",
+      error,
+    });
+  }
+};
+
 module.exports = {
   loginController,
   registerController,
   getUserDataController,
   applyDoctorController,
+  getAllNotificationController,
+  deleteAllNotificationController,
 };
