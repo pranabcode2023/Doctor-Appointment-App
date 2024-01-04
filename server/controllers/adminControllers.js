bc67c1315068ad64d0f73bf35b4c32802cfdf64a
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2024-01-04 15:24:11 +0100

    function added for getAllUsersController, getAllDoctorsController

---
+++ server/controllers/adminControllers.js
@@ -0,0 +1,41 @@
+const doctorModel = require("../models/doctorModel");
+const userModel = require("../models/userModels");
+//get all users
+const getAllUsersController = async (req, res) => {
+  try {
+    const users = await userModel.find({});
+    res.status(200).send({
+      success: true,
+      message: "Users data",
+      data: users,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error while fetching users",
+      error, //get all doctors
+    });
+  }
+};
+
+//get all doctors
+const getAllDoctorsController = async (req, res) => {
+  try {
+    const doctors = await doctorModel.find({});
+    res.status(200).send({
+      success: true,
+      message: "Doctors data",
+      data: doctors,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error while fetching doctors",
+      error,
+    });
+  }
+};
+
+module.exports = { getAllUsersController, getAllDoctorsController };
