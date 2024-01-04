17386334d7c19fe8fd972043cc47570480fe8a89
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2024-01-04 11:19:36 +0100

    Base Url put in env file

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -15,7 +15,8 @@ const Register = () => {
     try {
       dispatch(showLoading());
       const res = await axios.post(
-        "http://localhost:8080/api/v1/user/register",
+        // "http://localhost:8080/api/v1/user/register",
+        `${process.env.REACT_APP_BASE_URL}/api/v1/user/register`,
         values
       );
 
