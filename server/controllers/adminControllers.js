b4bb0600a473d63281c6882578b07eac6974e402
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2024-01-04 19:45:17 +0100

    changeAccountStatusController function added

--- server/controllers/adminControllers.js
+++ server/controllers/adminControllers.js
@@ -38,4 +38,39 @@ const getAllDoctorsController = async (req, res) => {
   }
 };
 
-module.exports = { getAllUsersController, getAllDoctorsController };
+//Doctor Account Status Controller
+const changeAccountStatusController = async (req, res) => {
+  try {
+    const { doctorId, status } = req.body;
+    const doctor = await doctorModel.findByIdAndUpdate(doctorId, { status });
+    const user = await userModel.findOne({ _id: doctor.userId });
+
+    const notification = user.notification;
+    notification.push({
+      type: "Doctor Account request updated",
+      message: `Your Doctor Account Request has been ${status}`,
+      onClickPath: "/notification",
+    });
+
+    user.isDoctor === "accepted" ? true : false;
+    await user.save();
+    res.status(201).send({
+      success: true,
+      message: "Account Status Updated",
+      data: doctor,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in Doctor account status",
+      error,
+    });
+  }
+};
+
+module.exports = {
+  getAllUsersController,
+  getAllDoctorsController,
+  changeAccountStatusController,
+};
