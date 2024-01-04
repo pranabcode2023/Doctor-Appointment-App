b8b4f4fe2ca059ca44502cfd54bd6f04a23193cb
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2024-01-04 16:55:34 +0100

    function added to getDoctors

--- client/src/pages/admin/Doctors.js
+++ client/src/pages/admin/Doctors.js
@@ -1,10 +1,80 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/Layout";
+import axios from "axios";
+import { Table } from "antd";
 
 const Doctors = () => {
+  const [doctors, setDoctors] = useState([]);
+
+  // get All users
+  const getDoctors = async () => {
+    try {
+      const res = await axios.get(
+        `${process.env.REACT_APP_BASE_URL}/api/v1/admin/getAllDoctors`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+
+      if (res.data.success) {
+        setDoctors(res.data.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getDoctors();
+  }, []);
+
+  //antD table Columns
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      render: (text, record) => (
+        <span>
+          {record.firstName} {record.lastName}
+        </span>
+      ),
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+    },
+    {
+      title: "Phone",
+      dataIndex: "phone",
+    },
+
+    {
+      title: "Status",
+      dataIndex: "status",
+    },
+
+    {
+      title: "Actions",
+      dataIndex: "actions",
+      render: (text, record) => (
+        <div className="d-flex">
+          {record.status === "pending" ? (
+            <button className="btn btn-success">Accept</button>
+          ) : (
+            <button className="btn btn-danger">Reject</button>
+          )}
+        </div>
+      ),
+    },
+  ];
+
   return (
     <Layout>
       <h1>All Doctors</h1>
+      <Table columns={columns} dataSource={doctors} />
     </Layout>
   );
 };
