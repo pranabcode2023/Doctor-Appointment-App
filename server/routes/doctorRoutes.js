b120a28709c9306ab47936edc7a9b32e440e6a5f
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2024-01-05 17:32:20 +0100

    route to update doctor Profile

--- server/routes/doctorRoutes.js
+++ server/routes/doctorRoutes.js
@@ -2,11 +2,20 @@ const express = require("express");
 const { authMiddleware } = require("../middlewares/authMiddleware");
 const {
   getDoctorProfileController,
+  updateDoctorProfileController,
 } = require("../controllers/doctorController");
 
 const router = express.Router();
 
 // Single doctor info( post method)
 router.post("/getDoctorProfile", authMiddleware, getDoctorProfileController);
+
+// update doctor Profile
+
+router.post(
+  "/updateDoctorProfile",
+  authMiddleware,
+  updateDoctorProfileController
+);
 
 module.exports = router;
