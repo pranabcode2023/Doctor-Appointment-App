a2b2650d658ac61f6e0691e0e87b6882133bf548
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2024-01-11 09:58:17 +0100

    route for DoctorAppointmentsPage added

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Doctors from "./pages/admin/Doctors";
 import Profile from "./pages/doctor/Profile";
 import BookingPage from "./pages/BookingPage";
 import AppointmentsPage from "./pages/AppointmentsPage";
+import DoctorAppointmentsPage from "./pages/doctor/DoctorAppointmentsPage";
 
 function App() {
   const { loading } = useSelector((state) => state.alerts);
@@ -89,6 +90,14 @@ function App() {
               element={
                 <ProtectedRoute>
                   <AppointmentsPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/doctor-appointments"
+              element={
+                <ProtectedRoute>
+                  <DoctorAppointmentsPage />
                 </ProtectedRoute>
               }
             />
