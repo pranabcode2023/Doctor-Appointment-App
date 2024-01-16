0b20527c9119e8dba578b7a7d817aa7719f330c5
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2024-01-16 09:17:05 +0100

    url in env

--- client/src/vercelConfig/serverURL.js
+++ client/src/vercelConfig/serverURL.js
@@ -1,14 +1,14 @@
-const serverURL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:8080"
-    : "https://doctor-appointment-app-server.vercel.app";
-
-export { serverURL };
-
-//NOTE -  env config vercel url
 // const serverURL =
 //   process.env.NODE_ENV === "development"
-//     ? `${process.env.REACT_APP_LOCALHOST_SERVER_URL}`
-//     : `${process.env.REACT_APP_VERCEL_SERVER_URL}`;
+//     ? "http://localhost:8080"
+//     : "https://doctor-appointment-app-server.vercel.app";
 
 // export { serverURL };
+
+//NOTE -  env config vercel url
+const serverURL =
+  process.env.NODE_ENV === "development"
+    ? `${process.env.REACT_APP_LOCALHOST_SERVER_URL}`
+    : `${process.env.REACT_APP_VERCEL_SERVER_URL}`;
+
+export { serverURL };
